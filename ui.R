b52b410f6f813d6b75af82641572e4ed73e63da1
ignacio gamez moll <ignacio@MacBook-Pro-de-ignacio.local>
2018-01-23 02:21:22 +0100

    Hemos incorporado los textos de ayuda para los selecInput y el actionBoton

--- ui.R
+++ ui.R
@@ -10,15 +10,24 @@ ui <- shinyUI(fluidPage(
   titlePanel("Buscador de Vuelos"),
   
   sidebarLayout(
+    
     sidebarPanel(
+      
+      helpText("Selecciona uno de los aeropuertos del despegable como origen, la opción --TODOS-- será como
+               si seleccionaras todos los aeropuertos"),
       selectInput("despegableOrigen", 
                   label = "ORIGEN",
                   choices = choicesAeropuertos,
                   selected = "TODOS"),
+      br(),
+      helpText("Selecciona uno de los aeropuertos del despegable como destino, si eliges --TODOS-- 
+               será como si seleccionaras todos los destinos podibles"),
       selectInput("despegableDestino",
                   label = "DESTINO",
                   choices = choicesAeropuertos,
                   selected = "TODOS"),
+      br(),
+      helpText("Púlsa el botón BUSCAR para comenzar la búsqueda"),
       actionButton("go", "BUSCAR")
     ),
     
